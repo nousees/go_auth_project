14d4c12bcd75af6dd640f9a34f7f7cb391f1478c
Danila <danil01062004@mail.ru>
2025-03-22 10:22:55 +0400

    hotfix

--- internal/entities/user/user.go
+++ internal/entities/user/user.go
@@ -2,6 +2,6 @@ package entities
 
 type User struct {
 	ID       int64  `json:"id" gorm:"primaryKey:autoIncrement"`
-	Email    string `json:"email" gorm:"not null"`
+	Email    string `json:"email" gorm:"not null;uniqueIndex"`
 	Password string `json:"password" gorm:"not null"`
 }
